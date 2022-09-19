0d8f17e97dddf18b0f6f308ab47a0d9a41bcaa41
Vinh Ta <vinhtaca@hotmail.com>
2022-09-19 16:26:18 -0400

    testing text

--- wsgi.py
+++ wsgi.py
@@ -3,7 +3,7 @@ application = Flask(__name__)
 
 @application.route("/")
 def hello():
-    return "Hello World, Vinh!"
+    return "Hello World, Vinh 2!"
 
 if __name__ == "__main__":
     application.run()
